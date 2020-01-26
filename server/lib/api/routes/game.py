2e71cb35347ddbd73ee3b9255d38a013dac2240c
Andrew Wong <andrew.j.wong@outlook.com>
2020-01-26 22:15:50 +1100

    V2 question routes

--- server/lib/api/routes/game.py
+++ server/lib/api/routes/game.py
@@ -7,52 +7,71 @@ from sqlite3 import IntegrityError
 
 from ...authSession.methods import authenticated
 
-@routing.GET("/leaderboard.json")
-def leaderboard(self: RequestHandler, args: dict):
+
+@routing.GET("/scores.json")
+def scores(self: RequestHandler, args: dict):
     questionsSQL = ctfSQLMethod.questions.getQuestions()
+
     solvesSQL = ctfSQLMethod.questions.getSolves()
+
     usersSQL = authSQLMethod.getUsers()
 
     pointsMap = {}
     for question in questionsSQL:
         pointsMap[question[0]] = question[3]
 
-    leaderboard = {}
+    scores = {}
     for user in usersSQL:
-        leaderboard[user[0]] = dict(name=user[2] or user[1],  # user might not have a display name?
-                                    points=0)
+        scores[user[0]] = dict(name=user[2] or user[1],  # user might not have a display name?
+                               points=0)
 
     for solve in solvesSQL:
         try:
-            leaderboard[solve[0]]["points"] += pointsMap[solve[1]]
+            scores[solve[0]]["points"] += pointsMap[solve[1]]
         except:
             pass
 
-    return self.finish(JSON.DATA(leaderboard))
+    return self.finish(JSON.DATA(scores))
 
 
-@routing.POST("/ctf/userSolves.json")
+@routing.GET("/solves.json")
 @authenticated
-def userSolves(self: RequestHandler, args: dict):
-    return self.finish(JSON.data(ctfSQLMethod.questions.getSolves(user=self.current_user.id)))
+def getSolves(self: RequestHandler, args: dict):
+    if "question" in args:
+        solves = ctfSQLMethod.questions.getSolves(question=args["question"])
+        return self.finish(JSON.DATA(dict(count=len(solves))))
+
+    if "user" in args:
+        solves = ctfSQLMethod.questions.getSolves(user=args["user"])
+        return self.finish(JSON.DATA(solves))
+
+    solves = ctfSQLMethod.questions.getSolves()
+    solvesMap = {}
+    for userID, questionID in solves:
+        if questionID not in solvesMap:
+            solvesMap[questionID] = 0
+        solvesMap[questionID] += 1
+
+    return self.finish(JSON.DATA(solvesMap))
 
 
-@routing.POST("/ctf/questionSolves.json")
-@authenticated
-def questionSolves(self: RequestHandler, args: dict):
-    return self.finish(JSON.data(len(ctfSQLMethod.questions.getSolves(question=args["question"]))))
-
-
-@routing.POST("/ctf/solve")
+@routing.POST("/question/solve")
 @authenticated
 def trySolve(self: RequestHandler, args: dict):
-    if args["flag"] == ctfSQLMethod.questions.getFlag(args["question"]):
-        try:
-            ctfSQLMethod.questions.solveQuestion(
-                self.current_user.id, args["question"])
-            SSE_messages.addMessage(
-                self.current_user.name + " has found a flag!")
-        except IntegrityError:
-            pass
-        return self.finish(JSON.YES())
-    return self.finish(JSON.NO())
+
+    if self.current_user["id"] == 0:
+        self.finish(JSON.ERROR("Admin cannot participate in challenges"))
+        return
+
+    flag = args["flag"]
+    question = args["question"]
+    if flag != ctfSQLMethod.questions.getFlag(question):
+        return self.finish(JSON.FALSE())
+
+    try:
+        ctfSQLMethod.questions.solveQuestion(
+            self.current_user["id"], question)
+    except IntegrityError:
+        pass
+
+    return self.finish(JSON.TRUE())
