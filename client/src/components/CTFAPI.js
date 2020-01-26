2856398cdbd2ffed7dc818f8aa4180bde068772b
Andrew Wong <andrew.j.wong@outlook.com>
2020-01-27 03:12:52 +1100

    Update CTFAPI interface

--- client/src/components/CTFAPI.js
+++ client/src/components/CTFAPI.js
@@ -1,30 +1,43 @@
 const ADDRESS = 'http://localhost:8000'
-const API_VERSION = 'v1'
+const API_VERSION = 'v2'
+
+const fetch = require('node-fetch')
+
+const call = function (endpoint, data = {}) {
+  return fetch(endpoint[1], {
+    method: endpoint[0],
+    body: JSON.stringify(data)
+  }).then(r => r.json())
+}
 
 const craftURL = function (endpoint) {
   return `${ADDRESS}/api/${API_VERSION}/${endpoint}`
 }
 
 module.exports = {
-  GET: {
-    URL_GET_QUESTIONS: craftURL('ctf/questions.json'),
-    URL_GET_CATEGORIES: craftURL('ctf/categories.json'),
-    URL_GET_SOLVES: craftURL('ctf/solves.json'),
-    URL_GET_ALL_SOLVES: craftURL('ctf/adminSolves.json'),
-    URL_GET_QUESTION_SOLVES: craftURL('ctf/questionSolves.json'),
-    URL_GET_MY_SOLVES: craftURL('ctf/userSolves.json'),
-    URL_GET_LEADERBOARD: craftURL('ctf/leaderboard.json')
-  },
-  POST: {
-    URL_GET_FLAG: craftURL('ctf/question/getFlag'),
-    URL_EDIT_FLAG: craftURL('ctf/question/editFlag'),
-    URL_SOLVE_QUESTION: craftURL('ctf/solve'),
-    URL_AUTH_ME: craftURL('auth/me'),
-    URL_AUTH_AVAILABLE: craftURL('auth/usernameAvailable'),
-    URL_AUTH_REGISTER: craftURL('auth/register'),
-    URL_AUTH_LOGIN: craftURL('auth/login')
-  },
+  call,
 
-  // TODO: Uhm.
-  URL_uhmmmiforgot: craftURL('ctf/question/{slug}')
+  SOLVE_QUESTION: ['POST', craftURL('question/solve')],
+  GET_SCORES: ['GET', craftURL('scores.json')],
+  GET_SOLVES: ['GET', craftURL('solves.json')],
+
+  GET_USERS: ['GET', craftURL('auth')],
+  CREATE_USER: ['POST', craftURL('auth')],
+  DELETE_USER: ['DELETE', craftURL('auth')],
+  LOGIN: ['POST', craftURL('auth/login')],
+  LOGOUT: ['POST', craftURL('auth/logout')],
+  USERNAME_AVAILABE: ['POST', craftURL('auth/usernameAvailable')],
+  CHANGE_PASSWORD: ['PUT', craftURL('auth')],
+  // CHANGE_DISPLAY_NAME: ["PUT", craftURL("auth")],
+
+  GET_CATEGORIES: ['GET', craftURL('categories.json')],
+  CREATE_CATEGORY: ['POST', craftURL('category')],
+  UPDATE_CATEGORY: ['PUT', craftURL('category')],
+  DELETE_CATEGORY: ['DELETE', craftURL('category')],
+
+  GET_QUESTIONS: ['GET', craftURL('questions.json')],
+  GET_QUESTION_FLAG: ['POST', craftURL('question/getFlag')],
+  CREATE_QUESTION: ['POST', craftURL('question')],
+  UPDATE_QUESTION: ['PUT', craftURL('question')],
+  DELETE_QUESTION: ['DELETE', craftURL('question')]
 }
